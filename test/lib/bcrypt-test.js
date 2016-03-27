2c71e95f1f21a6fa3856338a9897bd2775d6abe1
Roman Klimenko <romaklimenko@gmail.com>
2016-03-27 21:08:19 +0200

    It turned out that mocha supports promises

--- test/lib/bcrypt-test.js
+++ test/lib/bcrypt-test.js
@@ -1,38 +1,33 @@
 const assert = require('assert')
 const bcrypt = require('../../lib/bcrypt')
-const _bcrypt = require('bcrypt')
 
 const password = 'passw0rd'
 const wrong_password = 'passw0rD'
 
 describe('lib/bcrypt', () => {
   describe('compare(password, hash)', () => {
-    it('should compare correct password with hash and return true', (done) => {
-      _bcrypt.hash(password, 10, (err, hash) => {
-        bcrypt.compare(password, hash).then((value) => {
-          assert.equal(value, true)
-          done()
+    it('should compare correct password with hash and return true', () => {
+      return bcrypt.hash(password).then((hashed_password) => {
+        return bcrypt.compare(password, hashed_password).then((result) => {
+          assert.equal(result, true)
         })
       })
-    }),
+    })
 
-    it('should compare incorrect password with hash and return false', (done) => {
-      _bcrypt.hash(wrong_password, 10, (err, hash) => {
-        bcrypt.compare(password, hash).then((value) => {
-          assert.equal(value, false)
-          done()
+    it('should compare incorrect password with hash and return false', () => {
+      return bcrypt.hash(password).then((hashed_password) => {
+        return bcrypt.compare(wrong_password, hashed_password).then((result) => {
+          assert.equal(result, false)
         })
       })
     })
   })
 
   describe('hash(password)', () => {
-    it('should hash password', (done) => {
-      bcrypt.hash(password).then((value) => {
-        _bcrypt.compare(password, value, (err, res) => {
-          assert.equal(err, undefined)
-          assert.equal(res, true)
-          done()
+    it('should hash password', () => {
+      return bcrypt.hash(password).then((hashed_password) => {
+        return bcrypt.compare(password, hashed_password).then((result) => {
+          assert.equal(result, true)
         })
       })
     })
