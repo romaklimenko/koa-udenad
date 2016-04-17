a70c9c25a488a2027b4c1a62f6a64d10a20d63ce
Roman Klimenko <romaklimenko@gmail.com>
2016-04-17 23:49:31 +0200

    koa-mongo

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
+const config = require('./config')
 const koa = require('koa')
+const MongoClient = require('mongodb').MongoClient
+const mongo = require('koa-mongo');
 const router = require('koa-router')()
 const session = require('koa-session')
 
 const app = koa()
 
-// session
-app.keys = ['Næ næ næ næ næ, det må vi ikke']
-app.use(session(app))
+app.use(mongo({
+  uri: config.db_url,
+  max: 100,
+  min: 1,
+  timeout: 10,
+  log: false
+}))
 
+app.use(function *(next){
+  // this is how we access database in middleware
+  // this.mongo.collection('users').findOne({}, (err, doc) => {
+  //   console.log(doc)
+  // })
+  const start = new Date
+  yield next
+  const ms = new Date - start
+  console.log('%s %s - %s', this.method, this.url, ms)
+})
+
+// session
+app.keys = config.keys
+app.use(session(app))
 
 // router
 router.get('/', function* (next) {
